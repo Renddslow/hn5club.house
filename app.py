c1ecfd70d7cf8a8c91503796603c3c435d71b23f
Matt McElwee <mattmcelwee@porcupineinc.com>
2017-07-05 13:54:03 +0000

    Added tons of stuff...

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 from xml.etree import ElementTree
 from xml.etree.ElementTree import Element, SubElement, Comment
 from xml.dom import minidom
+from dateutil import tz
 import datetime
 import uuid
 import json
 import random
 
+import requests
 from flask import Flask, jsonify, request, render_template, Response, g
+import redis
 
 from lonely import Lonely
 from models import DATABASE
@@ -33,6 +36,7 @@ application.register_blueprint(dungeon_api)
 def before_request():
 	g.db = DATABASE
 	g.db.connect()
+	g.r = redis.StrictRedis(host='localhost', port=6379, db=2)
 
 @application.after_request
 def after_request(response):
@@ -41,7 +45,78 @@ def after_request(response):
 
 @application.route("/")
 def home():
-	return "<h1 style='text-align: center; width: 100%;'>Welcome to HN5 Clubhouse. Where the karma is cheap and the laughs are hearty.</h1>"
+	title = "HN5 Clubhouse"
+	footer = "Check out the <a href='/legacy'>wall of legacy</a>"
+	return render_template("countdown.html", title=title, footer=footer)
+
+
+@application.route("/legacy")
+def legacy():
+	title = "The Wall of Legacy | HN5 Clubhouse"
+	people = requests.get("http://hn5club.house/api/v1/legacy").json()['people']
+	footer = "<a href='/'>Countdown</a> to the immeninent demise of HN5"
+	return render_template("legacy.html", title=title, footer=footer, people=people)
+
+
+@application.route("/api/v1/daysLeft")
+def daysLeft():
+	from_zone = tz.gettz("UTC")
+	to_zone = tz.gettz("America/Chicago")
+	
+	then_utc = datetime.datetime(2017,7,6,16,0,0)
+	then_utc = then_utc.replace(tzinfo=from_zone)
+	
+	now_utc = datetime.datetime.now()
+	now_utc = now_utc.replace(tzinfo=from_zone)
+
+	then_cdt = then_utc.astimezone(to_zone)
+	now_cdt = now_utc.astimezone(to_zone)
+	
+	delta = then_cdt - now_cdt
+	
+	if delta.days < 0:
+		message = "HN5 No Longer Exists"
+	else:
+		message = delta.days
+
+	return jsonify({"daysLeft": message})
+
+
+@application.route("/api/v1/legacy", methods=['GET','POST'])
+def legacy_api():
+	if request.method == "GET":
+		people = g.r.lrange("hn5:people:legacy", 0, -1)
+		return jsonify({"people": people})
+	else:
+		form = request.form
+		for name in form['names'].split(","):
+			g.r.rpush("hn5:people:legacy", name.strip())
+
+		return jsonify({"message": "They have been added to the wall of legacy. They will be missed."})
+
+
+@application.route("/api/v1/inspire", methods=['GET','POST'])
+def inspire():
+	image = requests.get("http://inspirobot.me/api?generate=true").text
+	message = {
+		"style": "image",
+		"id": str(uuid.uuid4()),
+		"url": image,
+		"title": "Be inspired",
+		"thumbnail": {
+			"url": image,
+			"url@2x": image,
+			"width": 650,
+			"height": 650
+		}
+	}
+	card = {
+		"color": "red",
+		"message": image,
+		"message_format": "text",
+		"notify": True
+	}
+	return jsonify(card)
 
 
 @application.route("/api/v1/lonely/subscribe", methods=['GET', 'POST'])
